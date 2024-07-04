6d4499244426a5157648f9368484287f4253a252
dfrancohdez <dfrancohdez@gmail.com>
2024-07-03 21:33:55 -0600

    dvd v1

---
+++ src/DVD.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import "./_style.scss"
+let direccionX = false//debe de ir afuera porque se renderiza
+let direccionY = false
+let colorNum = 0
+let aux = true;
+let aux2 = true;
+const doc = document.getElementById("container")//necesario
+
+
+export const DVD = () => {
+    
+
+    const [valueX, setValueX] = useState(0)
+    const [valueY, setValueY] = useState(0)
+    const colorArray = ["red", "green", "blue", "yellow", "purple", "orange", "aqua", "goldenrod"
+        , "greenyellow", "lime", "magenta", "olive", "silver", "fuchsia", "firebrick", "darkblue", "cadetblue",
+        "aquamarine"
+    ]
+    const [color, setColor] = useState("gray")
+    //const [direccionX,setDireccionX]=useState(false)//no se porque no se puede con state
+    //const [direccionY,setDireccionY]=useState(false)
+    const cahngeColor = () => {
+        //colorNum=colorArray.length>colorNum?colorNum+1:0;
+        setColor(colorArray[colorNum])
+        colorNum = colorArray.length > colorNum + 1 ? colorNum + 1 : 0;
+
+        return;
+    }
+    useEffect( () => {
+        const height = document.getElementById("container").offsetHeight - 90;
+        const width = document.getElementById("container").offsetWidth - 130;//porque va aqui
+        const interval = setInterval(() => {
+
+            if (valueX < width && !direccionX) {
+                setValueX(prev => prev + 1);
+                aux = direccionX
+            } else {
+
+
+
+                direccionX = true
+
+            }
+            aux !== direccionX && cahngeColor()
+
+            if (0 < valueX && direccionX) {
+                setValueX(prev => prev - 1);
+                aux = direccionX
+            } else {
+
+                direccionX = false
+
+            }
+            aux !== direccionX && cahngeColor()
+            ///////////////////////////////////////////////////////////
+
+            if (valueY < height && !direccionY) {
+                setValueY(prev => prev + 1);
+                aux2 = direccionY
+            } else {
+                //cahngeColor()
+                direccionY = true
+            }
+            aux2 !== direccionY && cahngeColor()
+
+            if (0 < valueY && direccionY) {
+                setValueY(prev => prev - 1);
+                aux2 = direccionY
+            } else {
+                //cahngeColor()
+                direccionY = false
+            }
+            aux2 !== direccionY && cahngeColor()
+
+
+        }, 5);//velocidad
+
+        console.log(valueX + " " + valueY)
+
+        return () => { clearInterval(interval) }
+    }, [valueX, valueY])
+    return (
+        <div className="screen">
+            <div className="container" id="container">
+                <div style={{ background: `${color}`, transform: `translate(${valueX}px,${valueY}px)` }} className="cube">
+                    DVD
+                </div>
+            </div>
+        </div>
+    )
+}
